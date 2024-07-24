b3424411ba7b338d2ae998273667493c48485e85
Elena Krylova <Krylovae.a@yandex.ru>
2024-07-24 23:36:41 +0300

    В тесты добавлено открытие страницы браузера, удалены аргумкенты (имя фикстуры)

--- test_browers_should_find.py
+++ test_browers_should_find.py
@@ -1,14 +1,16 @@
 from selene import browser, be, have
 
 
-def test_google_search(browser_setup):
+def test_google_search():
+    browser.open('')
     # Вводим поисковый запрос и проверяем, результаты поиска соответствуют ожидаемым
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
-def test_search_no_expected_results(browser_setup):
+def test_search_no_expected_results():
+    browser.open('')
     # Вводим случайный набор символов, по которому не ожидаем результатов поиска
-    browser.element('[name="q"]').should(be.blank).type('fghjkhgfEFGhjnbvc456Fgn').press_enter()
-    browser.element('[id="botstuff"]').should(have.text('По запросу fghjkhgfEFGhjnbvc456Fgn ничего не найдено.'))
+    browser.element('[name="q"]').should(be.blank).type('jmhgnfdfdghfjhfmjh4r').press_enter()
+    browser.element('[id="botstuff"]').should(have.text('По запросу jmhgnfdfdghfjhfmjh4r ничего не найдено.'))
 
