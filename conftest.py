ca8daebd7c82143195f8a70868e13e0ed0dcbd15
Elena Krylova <Krylovae.a@yandex.ru>
2024-07-24 23:27:16 +0300

    Из фикстуры browser_setup удалено указание браузера и его открытие, добавлен browser.config.base_url и параметр autouse=True

--- conftest.py
+++ conftest.py
@@ -2,15 +2,13 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="function", autouse=True)
 def browser_setup():
-    # Открыть браузер на нужной странице
-    browser.config.browser_name = 'chrome'
     # Установить размер окна браузера
-    browser.config.window_width = 520
-    browser.config.window_height = 470
-    # Открыть выбранную страницу Google
-    browser.open('https://google.com')
+    browser.config.window_width = 1280
+    browser.config.window_height = 1024
+    # Установить base_url -  страницу Google
+    browser.config.base_url = 'https://google.com'
     yield
     browser.quit()
 
