1b80e260464baadf27dec51b668e2f6e491fc1fd
Elena Krylova <Krylovae.a@yandex.ru>
2024-07-24 22:06:02 +0300

    Исправлена фикстура brower_setup

--- conftest.py
+++ conftest.py
@@ -1,12 +1,18 @@
 import pytest
 from selene import browser
 
-@pytest.fixture(scope="function", autouse=True)
-def browser_setup():
-    # Установить размер окна браузера
-    browser.config.window_width = 1280
-    browser.config.window_height = 1024
 
-    # Закрыть браузер после выполнения теста
+@pytest.fixture(scope="function")
+def browser_setup():
+    # Открыть браузер на нужной странице
+    browser.config.browser_name = 'chrome'
+    # Установить размер окна браузера
+    browser.config.window_width = 520
+    browser.config.window_height = 470
+    # Открыть выбранную страницу Google
+    browser.open('https://google.com')
     yield
     browser.quit()
+
+
+
